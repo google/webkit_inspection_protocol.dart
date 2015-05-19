1b6109e76ac92aa7ab32527e4382afc7cd865cd4
Marc Fisher II <fisherii@google.com>
2015-05-19 16:21:00 -0700

    Change travis.sh to automatically run dartanalyzer against all libraries in lib, test, and bin.

--- tool/travis.sh
+++ tool/travis.sh
@@ -8,4 +8,6 @@
 set -e
 
 # Verify that the libraries are error free.
-dartanalyzer --fatal-warnings lib/webkit_inspection_protocol.dart
+# Verify that the libraries are error free.
+grep -Rl --include "*.dart" --exclude-dir="packages" '^library .*;$' lib/ test/ bin/ | \
+    xargs dartanalyzer --fatal-warnings
