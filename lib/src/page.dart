8b76dece9d918589deb512256fde9436fa51a302
Marc Fisher II <fisherii@google.com>
2015-05-19 17:30:02 -0700

    Add missing copyright notices to new files.

--- lib/src/page.dart
+++ lib/src/page.dart
@@ -1,3 +1,6 @@
+// Copyright 2015 Google. All rights reserved. Use of this source code is
+// governed by a BSD-style license that can be found in the LICENSE file.
+
 part of wip;
 
 class WipPage extends WipDomain {
