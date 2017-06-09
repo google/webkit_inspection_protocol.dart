27a446795c97bad3cf5b824045e991648e8e060b
Devon Carew <devoncarew@google.com>
2017-06-09 13:08:27 -0700

    fix an Iterable / List issue

--- lib/webkit_inspection_protocol.dart
+++ lib/webkit_inspection_protocol.dart
@@ -45,7 +45,7 @@ class ChromeConnection {
     var response = await getUrl('/json');
     var respBody = await UTF8.decodeStream(response);
     List<Map<String, String>> data = JSON.decode(respBody);
-    return data.map((m) => new ChromeTab(m));
+    return data.map((m) => new ChromeTab(m)).toList();
   }
 
   Future<ChromeTab> getTab(bool accept(ChromeTab tab),
